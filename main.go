deb1abd6c9263974530a8ac62039d17d886ad636
Maxime Moreillon <m.moreillon@gmail.com>
2025-02-14 11:41:30 +0900

    working as a middlware

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -13,6 +14,63 @@ import (
 	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/lestrrat-go/jwx/jws"
 )
+
+
+func middleware(next http.Handler) http.Handler {
+
+	ctx, _ := context.WithCancel(context.Background())
+
+	certsUrl := os.Getenv("JWKS_URI")
+
+
+	ar := jwk.NewAutoRefresh(ctx)
+	ar.Configure(certsUrl, jwk.WithMinRefreshInterval(15*time.Minute))
+
+	
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		authHeader := r.Header.Get("Authorization")
+
+		jwt := strings.Split(authHeader, "Bearer ")[1]
+
+		keyset, err := ar.Fetch(ctx, certsUrl)
+		if err != nil {
+			log.Panic("Error loading Fetching JWKS")
+		}
+
+		var hasVerified = false
+
+		// TODO: Just use the appropriate key
+		for it := keyset.Iterate(context.Background()); it.Next(context.Background()); {
+			pair := it.Pair()
+			key := pair.Value.(jwk.Key)
+		
+			var rawkey interface{} // This is the raw key, like *rsa.PrivateKey or *ecdsa.PrivateKey
+			if err := key.Raw(&rawkey); err != nil {
+				log.Printf("failed to create public key: %s", err)
+				break
+			}
+
+			_, err := jws.Verify([]byte(jwt), jwa.RS256, rawkey)
+
+			if err != nil {
+				
+			} else {
+				hasVerified = true
+			}
+		}
+
+		if(hasVerified) {
+			next.ServeHTTP(w, r)
+		} else {
+			fmt.Fprintf(w, "Invalid token")
+		}
+
+	})
+
+}
+
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
@@ -24,45 +82,16 @@ func main () {
 		log.Fatal("Error loading .env file")
 	}
 
-	ctx, _ := context.WithCancel(context.Background())
-
-	certsUrl := os.Getenv("JWKS_URI")
-	jwt := os.Getenv("JWT")
-
-
-	ar := jwk.NewAutoRefresh(ctx)
-	ar.Configure(certsUrl, jwk.WithMinRefreshInterval(15*time.Minute))
-
-	keyset, err := ar.Fetch(ctx, certsUrl)
-
-	for it := keyset.Iterate(context.Background()); it.Next(context.Background()); {
-		pair := it.Pair()
-		key := pair.Value.(jwk.Key)
-	
-		var rawkey interface{} // This is the raw key, like *rsa.PrivateKey or *ecdsa.PrivateKey
-		if err := key.Raw(&rawkey); err != nil {
-		  log.Printf("failed to create public key: %s", err)
-		  return
-		}
-
-		verified, err := jws.Verify([]byte(jwt), jwa.RS256, rawkey)
-
-		if err != nil {
-			log.Printf("failed to verify message: %s", err)
-		  } else {
-			log.Printf("signed message verified! -> %s", verified)
-
-		}
-
-	
-	  }
-
-
 	
 
 
-	http.HandleFunc("/", handler)
-	// fmt.Println("Server started at http://localhost:8080")
-	// log.Fatal(http.ListenAndServe(":8081", nil))
+	mux := http.NewServeMux()
+
+	finalHandler := http.HandlerFunc(handler)
+	mux.Handle("/", middleware(finalHandler))
+
+
+	fmt.Println("Server started")
+	log.Fatal(http.ListenAndServe(":8081", mux))
 
 }
