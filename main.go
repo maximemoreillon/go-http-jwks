25503d95c02b45624c1f99a03132f51f90861d23
Kevin Fabel <kevin_fabel@jtekt.co.jp>
2025-03-28 11:15:02 +0900

    Update lestrrat-go package version

--- main.go
+++ main.go
@@ -1,98 +1,29 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strings"
-	"time"
+
+	"jwksExperiments/auth"
 
 	"github.com/joho/godotenv"
-	"github.com/lestrrat-go/iter/arrayiter"
-	"github.com/lestrrat-go/jwx/jwa"
-	"github.com/lestrrat-go/jwx/jwk"
-	"github.com/lestrrat-go/jwx/jws"
 )
-
-
-func middleware(next http.Handler) http.Handler {
-
-	ctx, _ := context.WithCancel(context.Background())
-
-	certsUrl := os.Getenv("JWKS_URI")
-
-
-	ar := jwk.NewAutoRefresh(ctx)
-	ar.Configure(certsUrl, jwk.WithMinRefreshInterval(15*time.Minute))
-
-	
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		authHeader := r.Header.Get("Authorization")
-
-		jwt := strings.Split(authHeader, "Bearer ")[1]
-		// TODO: handle case where no JWT is provided
-
-		keyset, err := ar.Fetch(ctx, certsUrl)
-		if err != nil {
-			log.Panic("Error loading Fetching JWKS")
-		}
-
-		// Finding key pair with RS256 algorithm
-		var pair *arrayiter.Pair
-		for it := keyset.Iterate(context.Background()); it.Next(context.Background()); {
-			alg := it.Pair().Value.(jwk.Key).Algorithm()
-			if alg == "RS256" {
-				pair = it.Pair()
-				break
-			}
-		}
-
-		var rawkey interface{}
-		key := pair.Value.(jwk.Key)
-		if err := key.Raw(&rawkey); err != nil {
-			fmt.Fprintf(w,"failed to create public key: %s", err)
-			return
-		}
-
-		_, verifyErr := jws.Verify([]byte(jwt), jwa.RS256, rawkey)
-
-		if verifyErr != nil {
-			fmt.Fprintf(w, "Invalid token")
-		} else {
-			next.ServeHTTP(w, r)
-		}
-
-
-
-	})
-
-}
-
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
 }
 
-func main () {
+func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	
-
-
 	mux := http.NewServeMux()
-
 	finalHandler := http.HandlerFunc(handler)
-	mux.Handle("/", middleware(finalHandler))
-
+	mux.Handle("/", auth.Middleware(finalHandler))
 
 	fmt.Println("Server started")
 	log.Fatal(http.ListenAndServe(":8081", mux))
-
 }
